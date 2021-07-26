3ba95a9435396a03283400542430bbc7e8128c7c
anastasiarazb <anastasia.v.razb@gmail.com>
2021-07-26 22:52:56 +0300

    first day

---
+++ src/0_chat_bot_v0_if_else.py
@@ -0,0 +1,12 @@
+user_text = 'закажи еду'  # П и п - это 2 разные буквы
+
+if user_text == 'привет':
+    print('Привет-привет!')
+# elif = else if
+elif user_text == 'пока':
+    print("До скорого!")
+
+elif user_text == 'закажи еду':
+    print("Пиццу или борщ?")
+else:
+    print("Не понимаю вас")
---
+++ src/2_chat_bot_v1_input.py
@@ -0,0 +1,12 @@
+user_text = input()  # Добавили интерактивный ввод
+
+if user_text == 'привет':
+    print('Привет-привет!')
+# elif = else if
+elif user_text == 'пока':
+    print("До скорого!")
+
+elif user_text == 'закажи еду':
+    print("Пиццу или борщ?")
+else:
+    print("Не понимаю вас")
